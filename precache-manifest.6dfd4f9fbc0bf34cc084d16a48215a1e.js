4352bbec09c1a8a965883d3e54c7b6d41e91815a
Michael Ortiz <micort35@gmail.com>
2019-10-06 17:10:00 -0400

    Updates

---
+++ precache-manifest.6dfd4f9fbc0bf34cc084d16a48215a1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5917633538b719c03dd3c6333583280d",
+    "url": "/birch-labs/index.html"
+  },
+  {
+    "revision": "62709078e7f51367bdd7",
+    "url": "/birch-labs/static/css/main.95806a4a.chunk.css"
+  },
+  {
+    "revision": "48bc45554847bc1b0554",
+    "url": "/birch-labs/static/js/2.07bf634a.chunk.js"
+  },
+  {
+    "revision": "62709078e7f51367bdd7",
+    "url": "/birch-labs/static/js/main.7cf776dc.chunk.js"
+  },
+  {
+    "revision": "519c391f99bc4b4f643b",
+    "url": "/birch-labs/static/js/runtime~main.c90147fd.js"
+  }
+]);
